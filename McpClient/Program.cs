cecaba59b5b1de708c11f1dc0bba9796168971f2
yatao.zhu <yatao.zhu@tiancity.com>
2026-05-08 17:49:02 +0800

    first commit

---
+++ McpClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+{
+    Name = "Everything",
+    Command = "dotnet run",
+    Arguments = ["--project", @"E:\Tiancity\DevCode\McpDemo\McpServer\McpServer.csproj"],
+});
+
+var client = await McpClient.CreateAsync(clientTransport);
+
+// Print the list of tools available from the server.
+foreach (var tool in await client.ListToolsAsync())
+{
+    Console.WriteLine($"{tool.Name} ({tool.Description})");
+}
+
+// Execute a tool (this would normally be driven by LLM tool invocations).
+var result = await client.CallToolAsync(
+    "echo",
+    new Dictionary<string, object?>() { ["message"] = "Hello MCP!" },
+    cancellationToken: CancellationToken.None);
+
+// echo always returns one and only one text content object
+Console.WriteLine(result.Content.OfType<TextContentBlock>().First().Text);
